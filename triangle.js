0e87da57d35c33b73aea31b23bbac5582e40012f
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-02-08 16:09:29 +0100

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,1 +1,14 @@
 for (let i = "#"; i.length <= 7 ; i = i + "#") console.log(i);
+
+/*
+
+l'ho fatta così per provare a vedere se potevo farla in una riga
+sennò l'avrei fatta così:
+
+let triangleRow = "";
+for (let i = 0; i <= 6 ; i++) {
+    triangleRow += "#";
+    console.log(triangleRow);
+}
+
+*/
